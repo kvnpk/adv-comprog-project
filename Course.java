9446f7b59e3fb066cd98440e99bdaf03584f1d0a
patsakornakanit-hash <patsakorn.akanit@gmail.com>
2026-04-13 15:57:32 +0700

    Update Course

--- Course.java
+++ Course.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Course {
     private String courseName;
     private String courseID;
     private List<Student> enrolledStudents;
+    private Map<Student, String> grades;
+    private List<Observer> observers;
+    private GradingStrategy gradingStrategy;
 
-    public Course(String courseName,String courseID) {
+    public Course(String courseName,String courseID,GradingStrategy gradingStrategy) {
         this.courseName = courseName;
         this.courseID = courseID;
+        this.gradingStrategy = gradingStrategy;
         this.enrolledStudents = new ArrayList<>();
+        this.grades = new HashMap<>();
+        this.observers = new ArrayList<>();
     }
 
     public String getCourseName() { return courseName; }
